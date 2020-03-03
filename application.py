787b2ef77ace84b37e9c7ce9ffa39b3e003054b4
LilYeezyG <lilYeezyG@gmail.com>
2020-03-03 22:14:04 +0300

    nothing much

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
+#in forms.py created two classes of forms
 from forms import RegistrationForm, LoginForm
+from flask import flash
 
 # app = Flask(__name__) # to make the app run without any
 app = Flask(__name__)
@@ -12,12 +14,19 @@ def hello():
 
 @app.route("/login")
 def login():
+    #assign a form class imported from forms.py
     form = LoginForm()
     return render_template("login.html", form = form)    
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
+    #assign a form class imported from forms.py
     form = RegistrationForm()
+
+    #validate form data
+    
+    #redirect to home page
+
     return render_template("register.html", form = form)    
 
 @app.route("/about")
